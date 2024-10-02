127089d7f8c4077461d0d2b92605ed7a7b24ce19
alina <a.sarzhanova@g.nsu.ru>
2024-10-02 13:41:50 +0200

    create RadioItem

--- lrModule/src/RadialMenu.java
+++ lrModule/src/RadialMenu.java
@@ -1,27 +1,27 @@
 import java.awt.*;
+import javax.swing.*;
 
-public class MenuForm {
-    private int buttonSize = 50;
+public class RadialMenu extends JPanel {
+    private int buttonSize = 1000;
 
-    public MenuForm() {
+    public RadialMenu() {
         setBackground(new Color(20, 176, 211));
-        
-
     }
 
-    public void paint(Graphics g) {
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.setReminderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        int width = getWidth();
-        int height = getHeight();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        createMenuButtons(g2);
     }
 
     private void createMenuButtons(Graphics2D g2) {
         int width = getWidth();
         int height = getHeight();
         int x = (width - buttonSize) / 2;
-        int y = (height - buttinZise) / 2;
-        g2.setColor(getBackground());
+        int y = (height - buttonSize) / 2;
+        g2.setColor(Color.WHITE);
         g2.fillOval(x, y, buttonSize, buttonSize);
     }
 }
